c3f6526de5564cde4d4e6390dd21f5ee4449982f
shashank singh <shashank.personal@gmail.com>
2012-06-06 17:23:57 +0530

    commting urls , empty view for userprofileapplication

--- pinry/userprofile/models.py
+++ pinry/userprofile/models.py
@@ -1,10 +1,72 @@
 from django.db import models
 from django.contrib.auth.models import User
      
-class UserProfile(models.Model):
-    url = models.URLField()
-    firstname = models.CharField()
-    lastname = models.CharField()
-    home_address = models.TextField()
-    phone_numer = models.PhoneNumberField()
+#class UserProfile(models.Model):
+#    url = models.URLField()
+#    firstname = models.CharField()
+#    lastname = models.CharField()
+#    home_address = models.TextField()
+#    phone_numer = models.PhoneNumberField()
+#    user = models.ForeignKey(User, unique=True)
+
+
+class FacebookProfileModel(models.Model):
+    '''
+    Abstract class to add to your profile model.
+    NOTE: If you don't use this this abstract class, make sure you copy/paste
+    the fields in.
+    '''
+    about_me = models.TextField(blank=True)
+    facebook_id = models.BigIntegerField(blank=True, unique=True, null=True)
+    access_token = models.TextField(blank=True, help_text='Facebook token for offline access')
+    facebook_name = models.CharField(max_length=255, blank=True)
+    facebook_profile_url = models.TextField(blank=True)
+    website_url = models.TextField(blank=True)
+    blog_url = models.TextField(blank=True)
+    image = models.ImageField(blank=True, null=True,upload_to=PROFILE_IMAGE_PATH, max_length=255)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=(('m', 'Male'), ('f', 'Female')), blank=True, null=True)
+    raw_data = models.TextField(blank=True)
     user = models.ForeignKey(User, unique=True)
+
+    def __unicode__(self):
+        return self.user.__unicode__()
+
+    class Meta:
+        abstract = True
+        
+    def likes(self):
+        likes = FacebookLike.objects.filter(user_id=self.user_id)
+        return likes
+    
+    def friends(self):
+        friends = FacebookUser.objects.filter(user_id=self.user_id)
+        return friends
+
+    def post_facebook_registration(self, request):
+        '''
+        Behaviour after registering with facebook
+        '''
+        from django_facebook.utils import next_redirect
+        default_url = reverse('facebook_connect')
+        response = next_redirect(request, default=default_url,
+                                 next_key='register_next')
+        response.set_cookie('fresh_registration', self.user_id)
+
+        return response
+    
+    def clear_access_token(self):
+        self.access_token = None
+        self.save()
+
+    def get_offline_graph(self):
+        '''
+        Returns a open facebook graph client based on the access token stored
+        in the user's profile
+        '''
+        from open_facebook.api import OpenFacebook
+        if self.access_token:
+            graph = OpenFacebook(access_token=self.access_token)
+            graph.current_user_id = self.facebook_id
+            return graph
+        
